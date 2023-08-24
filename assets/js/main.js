68a9382e145a417f55ece421fed4818ca706af33
Tarek Salla <tarek-salla.94@hotmail.com>
2023-08-24 16:18:13 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
+
+
+<script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+
+
+
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
